e1210dddf663e4732276e02abbb6e8a12e374f46
ChaudhryAsif <ma232505@gmail.com>
2024-11-25 10:23:11 +0500

    initial app call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PostService } from './service/post.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [PostService]
 })
+
 export class AppComponent {
   title = 'prj-app';
+public posts: any;
+  constructor(private post: PostService) {}
+
+ngOnInit(){
+   this.post.getPosts().subscribe(response => {
+    this.posts = response;
+    console.log('response', response)
+  })
+}
+
 }
